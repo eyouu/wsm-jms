371fa36654f4afb9dbef1c0da0a71fa96ea8c767
Vasyl <vazhovnir@gmail.com>
2021-12-19 16:28:28 +0200

    init

---
+++ src/main/java/com/whosaidmeow/wsmjms/WsmJmsApplication.java
@@ -0,0 +1,13 @@
+package com.whosaidmeow.wsmjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsmJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsmJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whosaidmeow/wsmjms/WsmJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whosaidmeow.wsmjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsmJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
